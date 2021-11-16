8717e81d0bf151c68704c35562b25d634b771cc8
htnabe <n21m312@matsu.shimane-u.ac.jp>
2021-11-16 15:54:11 +0900

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import * as Sentry from 'sentry-expo';
+
+Sentry.init({
+  dsn: 'https://0599de2ba5c4455eaae6a7ec55f74bb3@o1070044.ingest.sentry.io/6065581',
+  enableInExpoDevelopment: true,
+  debug: true, // Sentry will try to print out useful debugging information if something goes wrong with sending an event. Set this to `false` in production.
+});
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Button
+        title="throw error"
+        onPress={() => {
+          throw new Error('test error'); //button pushでエラー発生させる
+        }}
+      />
       <StatusBar style="auto" />
     </View>
   );
